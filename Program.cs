541bd02cc941201a7ad2ad2c0ccb7a415000eea1
Brent Welke <brent@theKingdomAge.com>
2024-11-02 18:12:53 -0700

    First commit
    Testing out https://github.com/Jurioli/Blazor.WebForm.Components in a BlazorWebApp.
    
    I had some difficulties so once I was done I wanted to share a working example.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using BlazorWebForm.Components;
+using Microsoft.AspNetCore.Components.Server.Circuits;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<CircuitHandler, ScriptManagerCircuitHandler>();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddHttpClient(); 
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
+
+
